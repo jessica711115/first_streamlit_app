b0e76356cc711b11f8435ab53c036f25c8317342
jessica711115 <141005927+jessica711115@users.noreply.github.com>
2023-07-31 15:31:49 +0800

    Update streaamlit_app.py
    added line to import streamlit
    added title

--- streaamlit_app.py
+++ streaamlit_app.py
@@ -1,13 +1,3 @@
 import streamlit
 
-streamlit.title ('My Parents New Healthy Diner')
-
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
-
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
-
+streamlit.title('My Parents New Healthy Diner')
